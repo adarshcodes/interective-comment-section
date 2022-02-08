3d659a5f2628eb086a6b05e777c8bac2e4236c7a
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-08 21:03:58 +0530

    adding main background color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import "./Assets/sass/main.css";
+import "./assets/sass/main.css";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="app">This is the Testing version of the site.</header>
+			<main className="app">This is the Testing version of the site.</main>
 		</div>
 	);
 }
