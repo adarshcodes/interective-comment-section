f38b40989d71ea47a715f51569a5936adc5a21cf
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-10 13:07:59 +0530

    updated counter

--- src/components/VoteCounter.jsx
+++ src/components/VoteCounter.jsx
@@ -9,13 +9,19 @@ import Minus from "../assets/images/icon-minus.svg";
 function VoteCounter(props) {
 	return (
 		<div className="vote-counter">
-			<div className="vote-counter__vote-icon" onClick={props.incre}>
+			<div
+				className="vote-counter__vote-icon vote-counter__upvote-icon"
+				onClick={props.incre}
+			>
 				<img src={Plus} alt="upvote" />
 			</div>
 
 			<p className="vote-counter__vote-count">{props.count}</p>
 
-			<div className="vote-counter__vote-icon" onClick={props.decre}>
+			<div
+				className="vote-counter__vote-icon vote-counter__upvote-icon"
+				onClick={props.decre}
+			>
 				<img src={Minus} alt="downvote" />
 			</div>
 		</div>
