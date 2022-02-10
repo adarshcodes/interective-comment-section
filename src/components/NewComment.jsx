6869bf709fd029143adcce8455f4124bca802972
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-10 21:48:36 +0530

    styled new comment component

--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -1,8 +1,28 @@
 // native import
 import React from "react";
 
+// image import
+import currentUser from "../assets/images/avatars/image-juliusomo.webp";
+
 function NewComment() {
-	return <div className="new-comment">This is a new comment card</div>;
+	return (
+		<div className="new-comment">
+			<img
+				src={currentUser}
+				alt="user-avatar"
+				className="new-comment__avatar"
+			/>
+			<div className="new-comment__form">
+				<textarea
+					name="add-comment"
+					id="add-comment"
+					className="new-comment__add-comment"
+					placeholder="Add a comment..."
+				></textarea>
+			</div>
+			<div className="new-comment__btn-comment">Send</div>
+		</div>
+	);
 }
 
 export default NewComment;
