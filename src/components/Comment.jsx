fcf4d9cb9c57aa9a53dd2141b2a311abc675beae
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-10 10:23:20 +0530

    image is not loading in comment card

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,24 +3,22 @@ import React from "react";
 
 // other component import
 import VoteCounter from "./VoteCounter";
-import UserData from "../userData";
 
 // image import
 import ReplyIcon from "../assets/images/icon-reply.svg";
-import Avatar from "../assets/images/avatars/image-amyrobson.webp";
 
-function Comment() {
+function Comment(props) {
 	const [vote, setVote] = React.useState(0);
 
 	function upvote() {
 		setVote((prevCount) => prevCount + 1);
-		console.log("It's working");
 	}
 
 	function downvote() {
 		setVote((prevCount) => prevCount - 1);
-		console.log("It's working");
 	}
+
+	console.log(props.avatar);
 
 	return (
 		<div className="comment">
@@ -29,11 +27,11 @@ function Comment() {
 			<div className="comment--user-details">
 				<div className="comment--user-info">
 					<div className="comment--data">
-						<img src={Avatar} alt="user-avatar" className="avatar" />
+						<img src={props.avatar} alt="user-avatar" className="avatar" />
 
-						<h3 className="username">amyrobson</h3>
+						<h3 className="username">{props.username}</h3>
 
-						<p className="timestamp">1 month ago</p>
+						<p className="timestamp">{props.timestamp}</p>
 					</div>
 					<div className="comment--reply">
 						<img src={ReplyIcon} alt="reply" className="reply-icon" />
@@ -41,11 +39,7 @@ function Comment() {
 					</div>
 				</div>
 
-				<p className="comment--comment-text">
-					Impressive! Though it seems the drag feature could be improved. But
-					overall it looks incredible. You’ve nailed the design and the
-					responsiveness at various breakpoints works really well.
-				</p>
+				<p className="comment--comment-text">{props.text}</p>
 			</div>
 		</div>
 	);
--- src/userData.js
+++ src/userData.js
@@ -72,5 +72,3 @@ const userData = [
 		],
 	},
 ];
-
-export default userData;
