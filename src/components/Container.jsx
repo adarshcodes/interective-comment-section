ba134b3ccd6005db111e27170a4629d5966574a3
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-10 13:44:12 +0530

    styled and mapped replies

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,21 +3,41 @@ import React from "react";
 
 // Component import
 import Comment from "./Comment";
+import Replies from "./Replies";
 import UserData from "../userData";
 
 function Container() {
 	const [commentsData] = React.useState(UserData.comments);
 
-	const comments = commentsData.map((comment) => (
-		<Comment
-			key={comment.id}
-			avatar={comment.user.image.webp}
-			username={comment.user.username}
-			timestamp={comment.createdAt}
-			text={comment.content}
-			upvote={comment.score}
-		/>
-	));
+	const comments = commentsData.map((comment) => {
+		const haveReply = comment.replies;
+
+		return (
+			<div className="comments-holder">
+				<Comment
+					key={comment.id}
+					avatar={comment.user.image.webp}
+					username={comment.user.username}
+					timestamp={comment.createdAt}
+					text={comment.content}
+					upvote={comment.score}
+				/>
+
+				{haveReply.length > 0
+					? haveReply.map((replies) => (
+							<Replies
+								key="id"
+								avatar={replies.user.image.webp}
+								username={replies.user.username}
+								timestamp={replies.createdAt}
+								text={replies.content}
+								upvote={replies.score}
+							/>
+					  ))
+					: null}
+			</div>
+		);
+	});
 
 	return <div className="container">{comments}</div>;
 }
