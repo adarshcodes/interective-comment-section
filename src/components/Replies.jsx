663f82b6f248d17dab045603462118e431b80ba3
Adarshcodes <adarsh.lrnr@gmail.com>
2022-02-11 13:50:17 +0530

    conditionally rendering update and edit button

--- src/components/Replies.jsx
+++ src/components/Replies.jsx
@@ -35,18 +35,18 @@ function Replies(props) {
 						<p className="timestamp">{props.timestamp}</p>
 					</div>
 
-					<div className="comment--update">
-						<div className="comment--update__delete">
-							<i class="fa-solid fa-trash"></i>
-							Delete
+					{props.username === props.superuser ? (
+						<div className="comment--update">
+							<div className="comment--update__delete">
+								<i class="fa-solid fa-trash"></i>
+								Delete
+							</div>
+							<div className="comment--update__edit">
+								<i class="fa-solid fa-pen"></i>
+								Edit
+							</div>
 						</div>
-						<div className="comment--update__edit">
-							<i class="fa-solid fa-pen"></i>
-							Edit
-						</div>
-					</div>
-
-					{props.username === props.superuser ? null : (
+					) : (
 						<div className="comment--reply">
 							<img src={ReplyIcon} alt="reply" className="reply-icon" />
 							Reply
