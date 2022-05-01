dfa16bbe5c720aab49be4dbfb178f3980883dc10
Adarshcodes <adarsh.lrnr@gmail.com>
2022-05-01 12:38:09 +0530

    strating up fresh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import "./assets/sass/main.css";
-import Container from "./components/Container";
 
 function App() {
 	return (
 		<div className="app-container">
-			<main className="app">
-				<Container />
-			</main>
+			<main className="app"></main>
 		</div>
 	);
 }
