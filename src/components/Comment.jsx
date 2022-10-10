e69c1c36384c2335599577584fff9fdef3c53618
Adarsh Pratap Singh <adarsh.lrnr@gmail.com>
2022-10-10 10:00:16 +0530

    rendering comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import "../assets/sass/main.css";
 import VoteCounter from "./VoteCounter";
+import Replies from "./Replies";
 
-export default function Comment({ content, time, score }) {
+export default function Comment({
+	username,
+	avatar,
+	content,
+	time,
+	score,
+	haveReplies,
+}) {
+	const replies = haveReplies.map((rep) => {
+		return (
+			<Replies
+				key={rep.id}
+				replyTime={rep.createdAt}
+				repliesScore={rep.score}
+				username={rep.user.username}
+				avatar={rep.user.image.webp}
+				repliesContent={rep.content}
+				replyTo={rep.replyingTo}
+			/>
+		);
+	});
 	return (
 		<div>
 			<VoteCounter score={score} />
-			<div>{content}</div>
+			<img src={avatar} alt="avatar" />
+			<div>{username}</div>
 			<div>{time}</div>
-			<div>{score}</div>
+			<div>{content}</div>
+			{haveReplies ? replies : null}
 		</div>
 	);
 }
